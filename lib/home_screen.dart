c5ea88d6fade9c8d8e7a3fe132d4a57b89d95148
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-03 09:43:56 -0500

    Read Provider using Consumer instead of ConsumerWidget

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,21 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:playground/main.dart';
 
-class MyHomePage extends ConsumerWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   // WidgetRef allows a widget to communicate to a provider
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final name = ref.watch(nameProvider);
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Column(
-          children: [
-            Center(
-              child: Text(name),
-            ),
-          ],
-        ));
+        body: Consumer(builder: (context, ref, child) {
+          final name = ref.watch(nameProvider);
+          return Column(
+            children: [
+              Center(
+                child: Text(name),
+              ),
+            ],
+          );
+        }));
   }
 }
