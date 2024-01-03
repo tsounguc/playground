669aabdf9c7f5e39cc5407c3d276e840bc9a7a03
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-03 09:21:08 -0500

    Make MyHomePage widget extend ConsumerWidget

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
