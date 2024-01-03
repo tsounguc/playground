3188a707c8846d1e63cf2ca577d18cb8e66239d7
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-03 09:18:40 -0500

    Create nameProvider Globally

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'home_screen.dart';
 
+final nameProvider = Provider<String>((ref) => 'Christian Tsoungui Nkoulou');
+
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
